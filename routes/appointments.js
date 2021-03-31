2cc6daddecdaab2ed7a9184935ff45fa21dbcb87
ChadNaps <napperc@hotmail.com>
2021-03-30 19:56:15 -0600

    Add different templates for admin and client roles

--- routes/appointments.js
+++ routes/appointments.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('pages/appointments/view-all', { title: "View All Appointments" });
+    if (req.app.locals.role === "admin") {
+        res.render('pages/appointments/view-all-admin', { title: "View All Appointments - Admin" });
+    } else {
+        res.render('pages/appointments/view-all-client', { title: "View All Appointments" });
+    }
 });
 
 module.exports = router;
