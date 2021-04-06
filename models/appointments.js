175a2463d3868218b101f70747af214809a33fca
ChadNaps <napperc@hotmail.com>
2021-04-06 12:53:10 -0600

    Update models/appointments.js - Make helper load function async

--- models/appointments.js
+++ models/appointments.js
@@ -7,32 +7,47 @@ module.exports = {
     /**
      * If user is set to 'admin', will return all appointments in db. Otherwise, user should be
      * set to the username of the currently logged in user. This function will then return
-     * all appointments booked for the specified user from the provided db.
+     * all appointments booked for the specified user from the db.
      *
-     * @param {object} db   The database to be queried
-     * @param {string} user The user whose schedule is to be found
-     * @return {array}     Array of appointments that were found for the user
+     * @param {object} db       The database to be queried
+     * @param {string} user     The user whose schedule is to be found
+     * @return {Promise<array>} Array of appointments that were found for the user, empty array if none were found
      */
-    load: function (db, user) {
+    load: async function (db, user) {
+        const adminQuery = "SELECT * FROM appointments";
+        const nonAdminQuery = "SELECT * FROM appointments WHERE u_id = ?";
+        const UIDLookupQuery = "SELECT u_id FROM authentication WHERE username = ?";
+        let u_id = "";
 
-        if (user === "admin") {
-            const query = "SELECT * FROM appointments";
-            db.all(query, [], (err, rows) => {
+        return new Promise((resolve, reject) => {
+            // Get ID of user if they aren't admin
+            if (user !== "admin") {
+                db.get(UIDLookupQuery, [user], (err, row) => {
+                    if (err) {
+                        return reject(err);
+                    } else {
+                        u_id = row.u_id;
+                    }
+                });
+            }
+
+            // Get appointments based on role or name of logged in user
+            db.all(user === "admin" ? adminQuery : nonAdminQuery, user === "admin" ? [] : [u_id], (err, rows) => {
                 if (err) {
-                    console.error(err.message);
+                    return reject(err);
                 } else {
-                    return rows;
+                    // Correctly format stringified JSON entries
+                    for (const row of rows) {
+                        if (row.geo) {
+                            row.geo = JSON.parse(row.geo);
+                        }
+                        if (row.organizer) {
+                            row.organizer = JSON.parse(row.organizer);
+                        }
+                    }
+                    return resolve(rows);
                 }
             });
-        } else {
-            const query = "SELECT * FROM appointments WHERE u_id = (SELECT u_id FROM authentication WHERE username = ?)";
-            db.all(query, [user], (err, rows) => {
-                if (err) {
-                    console.error(err.message);
-                } else {
-                    return rows;
-                }
-            });
-        }
+        });
     }
 };
